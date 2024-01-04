b861cf7c7cb4911bf833f521b1577a2911fb65c1
AjwadG <ajwadelgandouz@yahoo.com>
2024-01-04 13:55:41 +0200

    avl-insert fix

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -63,6 +63,47 @@ bst_t *bst_insert(bst_t **tree, int value)
 }
 
 /**
+ * avl_fix - dose the rotaions
+ *
+ * @root: root of the tree
+ *
+ * Return: the new root
+ */
+avl_t *avl_fix(avl_t **root)
+{
+	int bf, rbf, lbf;
+
+	if (!*root)
+		return (NULL);
+
+	if ((*root)->right)
+		(*root)->right = avl_fix(&(*root)->right);
+	if ((*root)->left)
+		(*root)->left = avl_fix(&(*root)->left);
+
+	bf = binary_tree_balance(*root);
+	rbf = binary_tree_balance((*root)->right);
+	lbf = binary_tree_balance((*root)->left);
+
+	if (bf > 1 && lbf == 1)
+		*root = binary_tree_rotate_right(*root);
+	else if (bf < -1 && rbf == -1)
+		*root = binary_tree_rotate_left(*root);
+	else if (bf < -1 && rbf == 1)
+	{
+		(*root)->right = binary_tree_rotate_right((*root)->right);
+		*root = binary_tree_rotate_left(*root);
+	}
+	else if (bf > 1 && lbf == -1)
+	{
+		(*root)->left = binary_tree_rotate_left((*root)->left);
+		*root = binary_tree_rotate_right(*root);
+	}
+
+	return (*root);
+}
+
+/**
  * avl_insert -  inserts a value in a AVL
  *
  * @tree: double pointer to the tree
@@ -72,39 +113,15 @@ bst_t *bst_insert(bst_t **tree, int value)
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *new, *tmp;
-	int bf, lbf, rbf;
+	avl_t *new;
 
 	if (!tree)
 		return (NULL);
 
 	new = bst_insert(tree, value);
-	for (tmp = new->parent; tmp; tmp = tmp->parent)
-	{
-		bf = binary_tree_balance(tmp);
-		if (tmp->right)
-			rbf = binary_tree_balance(tmp->right);
-		if (tmp->left)
-			lbf = binary_tree_balance(tmp->left);
+	if (!new)
+		return (NULL);
+	*tree = avl_fix(tree);
 
-		if (bf > 1 && lbf == 1)
-			tmp = binary_tree_rotate_right(tmp);
-		else if (bf < -1 && rbf == -1)
-			tmp = binary_tree_rotate_left(tmp);
-		else if (bf < -1 && rbf == 1)
-		{
-			tmp->right = binary_tree_rotate_right(tmp->right);
-			tmp = binary_tree_rotate_left(tmp);
-		}
-		else if (bf > 1 && lbf == -1)
-		{
-			tmp->left = binary_tree_rotate_left(tmp->left);
-			tmp = binary_tree_rotate_right(tmp);
-		}
-		else
-			continue;
-		if (*tree == tmp->left || *tree == tmp->right)
-			*tree = tmp;
-	}
 	return (new);
 }
